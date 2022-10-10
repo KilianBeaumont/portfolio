4d5efe1c9060711201051957c88efc138e483a93
Beaumont Kilian <kilian.beaumont@gmail.com>
2022-10-10 21:40:04 +0200

    dark mode + texte

--- anim.js
+++ anim.js
@@ -1,5 +1,7 @@
 const input = document.querySelector("input");
 const body = document.querySelector("body");
+const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
 
 const toggleThemeMode = () => {
     body.classList.toggle("dark");
